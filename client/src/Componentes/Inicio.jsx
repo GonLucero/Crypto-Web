200ac616faa635aaa49c37001cb8f8704e301f0d
gon.lucero <gonzalom.lucero@gmail.com>
2022-01-24 13:57:11 -0300

    funciona okkkk

--- client/src/Componentes/Inicio.jsx
+++ client/src/Componentes/Inicio.jsx
@@ -1,32 +1,23 @@
-// 11)  Creo mi componente de ingreso
-import React, { useState, useEffect} from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { getCryptos } from "../actions/index";
 import { Link } from "react-router-dom";
 import "../EstilosCss/Inicio.css"
-
+import back from "./images/wallpaper.jpg"
 
 export default function LandingPage() {
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    console.log('se ejecuta el dispatch')
-    dispatch(getCryptos());
-  }, []); 
+ 
 
   return (
-    <div class="background">
-      <div class="title" >
-        <h2>Welcome!</h2>
+      <div  style={{backgroundColor:'black' ,height:'0px', borderColor:'black'}}>
+      <img class="background" src={back} alt="" />
+
+  <div style={{backgroundColor:'black', height:'0px'}}>
         <Link to="/home">
-          <button type="submit">START</button>
+          <button className='button' type="submit">ENTER</button>
         </Link>
-        
-        <h3>Developed by: Gonzalo Lucero</h3>
-            
-      </div>
-      
-    </div>
+        </div>
+
+        </div>
+
+
   );
 }
